2a294a583c6603b2ba3167fa5ddbbc31301832c3
Mridul Sharma <22ucs127@lnmiit.ac.in>
2026-01-11 23:26:00 +0530

    Created pages folder to handle different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import React from 'react';
 import Navbar from './components/navbar';
-
+import { Home, Projects, Contact, About } from './pages';
 const App = () => {
     return (
-        <main className='bg-slate-300/20'>
+        <main className="bg-slate-300/20">
             <Router>
                 <Navbar />
                 <Routes>
-                    <Route path="/" element={'Home'}></Route>
-                    <Route path="/about" element={'About'}></Route>
-                    <Route path="/projects" element={'Projects'}></Route>
-                    <Route path="/contact" element={'Contact'}></Route>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/contact" element={<Contact />} />
                 </Routes>
             </Router>
         </main>
