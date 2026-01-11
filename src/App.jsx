53eb8646ae3a8886f2bbeec3def966b922fc7aba
Mridul Sharma <22ucs127@lnmiit.ac.in>
2026-01-11 21:51:27 +0530

    Create a Component called Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import React from 'react';
+import Navbar from './components/navbar';
 
 const App = () => {
     return (
-        <div className="min-h-screen bg-black flex items-center justify-center">
-            <h1 className="text-4xl font-bold text-blue-500">
-                Tailwind v4 is FINALLY working 🚀
-            </h1>
-        </div>
+        <main className='bg-slate-300/20'>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={'Home'}></Route>
+                    <Route path="/about" element={'About'}></Route>
+                    <Route path="/projects" element={'Projects'}></Route>
+                    <Route path="/contact" element={'Contact'}></Route>
+                </Routes>
+            </Router>
+        </main>
     )
 }
 
