f79a2aa50b61c42d3c86d853d083471175b4bd2c
Mridul Sharma <22ucs127@lnmiit.ac.in>
2026-01-12 22:04:59 +0530

    Added animation and rotation to plane model

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import { Gltf, useGLTF } from '@react-three/drei'
+import { React, useRef, useEffect } from 'react'
+import { Gltf, useAnimations, useGLTF } from '@react-three/drei'
 import planeObject from '../assets/3d/plane.glb'
 
 const Plane = ({ isRotating, ...props }) => {
+    const ref = useRef();
     const { scene, animations } = useGLTF(planeObject);
+    const { actions } = useAnimations(animations, ref);
+    useEffect(() => {
+        if (isRotating) {
+            actions['Take 001'].play();
+        } else {
+            actions['Take 001'].stop();
+        }
+    }, [isRotating, actions]);
+
     return (
-        <mesh {...props}>
+        <mesh {...props} ref={ref}>
             <primitive object={scene} />
         </mesh>
     )
