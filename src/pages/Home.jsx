b5548da733d70cf4ede299de9c661315723b0dca
Mridul Sharma <22ucs127@lnmiit.ac.in>
2026-01-12 22:36:53 +0530

    Sky modified and working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -79,14 +79,14 @@ const Home = () => {
                     <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
                     <Bird />
-                    <Sky />
+                    <Sky isRotating={isRotating} />
                     <Island
                         position={islandPosition}
                         scale={islandScale}
                         rotation={islandRotation}
                         isRotating={isRotating}
-                        setCurrentStage={setCurrentStage}
                         setIsRotating={setIsRotating}
+                        setCurrentStage={setCurrentStage}
                     />
                     <Plane
                         isRotating={isRotating}
