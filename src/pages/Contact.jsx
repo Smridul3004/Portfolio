46b87e28c37889c5be36f5aff1d10ab31fa591f6
Mridul Sharma <22ucs127@lnmiit.ac.in>
2026-01-15 02:05:59 +0530

    Added Fox model and features related to it on the contact page with different animations along with integrating email js feature.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,9 @@
 import { React, useState, useRef } from 'react'
 import emailjs from '@emailjs/browser';
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react';
+import { Fox } from '../models/fox.jsx';
+import Loader from '../components/Loader.jsx';
 
 const Contact = () => {
 
@@ -10,20 +14,25 @@ const Contact = () => {
         message: ''
     });
     const [isLoading, setIsLoading] = useState(false);
+    const [currentAnimation, setCurrentAnimation] = useState('Idle');
 
     const handleChange = (e) => {
         // Handle form input changes
         setFormData({ ...formData, [e.target.name]: e.target.value });
     }
     const handleFocus = () => {
-        // Handle input focus
+        setCurrentAnimation('walk');
+
     }
     const handleBlur = () => {
         // Handle input blur
+        setCurrentAnimation('Idle');
     }
+
     const handleSubmit = (e) => {
         e.preventDefault();
         setIsLoading(true);
+        setCurrentAnimation('hit');
         // Handle form submission
         emailjs.send(
             import.meta.env.VITE_EMAILJS_SERVICE_ID,
@@ -38,9 +47,15 @@ const Contact = () => {
         ).then(() => {
             setIsLoading(false);
             alert('Message sent successfully!');
-            setFormData({ name: '', email: '', message: '' });
+
+            setTimeout(() => {
+                setCurrentAnimation('Idle');
+                setFormData({ name: '', email: '', message: '' });
+            }, [3000]);
+
         }).catch((error) => {
             setIsLoading(false);
+            setCurrentAnimation('Idle');
             alert('Failed to send message. Please try again.');
             console.error('EmailJS error:', error);
         });
@@ -103,16 +118,37 @@ const Contact = () => {
                     </label>
                     <button
                         type='submit'
-                        className='btn-primary mt-6'
+                        className='btn mt-6'
                         disabled={isLoading}
                         onFocus={handleFocus}
                         onBlur={handleBlur}
-
                     >
                         {isLoading ? 'Sending...' : 'Send Message'}
                     </button>
 
                 </form>
+            </div>
+
+            <div className='lg:w-1/2 w-full h-[350px] md:h-[550px] lg:h-auto'>
+                <Canvas
+                    camera={{
+                        position: [0, 0, 5],
+                        fov: 75,
+                        near: 0.1,
+                        far: 1000
+                    }}
+                >
+                    <directionalLight intensity={2.5} position={[0, 0, 1]} />
+                    <ambientLight intensity={0.5} />
+                    <Suspense fallback={<Loader />}>
+                        <Fox
+                            currentAnimation={currentAnimation}
+                            position={[0.5, 0.35, 0]}
+                            rotation={[12.6, -0.6, 0]}
+                            scale={[0.5, 0.5, 0.5]}
+                        />
+                    </Suspense>
+                </Canvas>
             </div>
         </section >
     )
