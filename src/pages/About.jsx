c2c1286046b073cba1a98686ae623da35eefb89b
Mridul Sharma <22ucs127@lnmiit.ac.in>
2026-01-16 00:14:08 +0530

    Created About Page and included skills along with image icons.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { skills } from '../constants'
 
 const About = () => {
     return (
-        <div>About</div>
+        <section className='max-container'>
+            <h1 className='head-text'>
+                Hello I'm <span className='blue-gradient_text font-semibold drop-shadow-2xl'>Mridul Sharma</span>
+            </h1>
+
+            <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+                <p>Software Engineer based in India, specializing in web development and 3D design.</p>
+            </div>
+
+            <div className='py-10 flex flex-col'>
+                <h3 className='subhead-text'>My Skills</h3>
+
+                <div className='mt-16 flex flex-wrap gap-12'>
+
+                    {skills.map((skill, index) => (
+                        <div key={index} className='flex flex-col items-center gap-2'>
+                            <div className='block-container w-20 h-20'>
+                                <div className='btn-back rounded-xl' />
+                                <div className='btn-front rounded-xl flex justify-center items-center'
+                                >
+                                    <img
+                                        src={skill.imageUrl}
+                                        alt={skill.name}
+                                        className='w-10 h-10'
+                                    />
+                                </div>
+                            </div>
+                            <p className='text-sm text-center'>{skill.name}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className='py-16'>
+                <h3 className='subhead-text'>Work Experience</h3>
+                <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+                    <p>I've worked with all sorts of companies, leveling up my skillls
+                        and teaming up with some amazing people along the way.
+                        Here's the rundown:
+                    </p>
+                </div>
+            </div>
+        </section>
     )
 }
 
