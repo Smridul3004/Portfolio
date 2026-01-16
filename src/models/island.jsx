ee4d55bfaa14d2dc141a74aa740f9b787ff6c6de
Mridul Sharma <22ucs127@lnmiit.ac.in>
2026-01-16 22:05:44 +0530

    Modified Plane scale and position which looks great now

--- src/models/island.jsx
+++ src/models/island.jsx
@@ -60,12 +60,12 @@ const Island = ({ isRotating, setIsRotating, setCurrentStage, ...props }) => {
         if (e.key === 'ArrowLeft') {
             if (!isRotating) setIsRotating(true);
             islandRef.current.rotation.y += 0.01 * Math.PI;
-            // rotationSpeed.current = 0.007; // Store the rotation speed
+            rotationSpeed.current = 0.0125; // Store the rotation speed
         }
         else if (e.key === 'ArrowRight') {
             if (!isRotating) setIsRotating(true);
             islandRef.current.rotation.y -= 0.01 * Math.PI;
-            // rotationSpeed.current = -0.007; // Store the rotation speed
+            rotationSpeed.current = -0.0125; // Store the rotation speed
         }
     };
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Projects = () => {
     return (
-        <div>Projects</div>
+        <section className='max-container'>
+            <h1 className='head-text'>
+                My <span className='blue-gradient_text font-semibold drop-shadow-2xl'>Projects</span>
+            </h1>
+
+            <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+                <p>I enjoy building practical, user-focused software that solves real problems.
+                    My projects reflect hands-on experience with full-stack development, system design,
+                    and integrating modern tools like APIs and AI-driven workflows. I focus on writing
+                    clean, scalable code and turning ideas into reliable, working products.
+                </p>
+            </div>
+        </section>
     )
 }
 
