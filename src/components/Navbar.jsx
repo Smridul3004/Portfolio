d8a793846a2079a621006e91a83fc5b0ac040111
Mridul Sharma <22ucs127@lnmiit.ac.in>
2026-01-17 18:17:27 +0530

    Added mridul.svg file and replaced it with the logo for home button.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { useState, useEffect } from "react";
 
-import { logo } from "../assets/images";
+import { mridul } from "../assets/images";
 
 const Navbar = () => {
     const [scrolled, setScrolled] = useState(false);
@@ -18,7 +18,7 @@ const Navbar = () => {
     return (
         <header className={`header transition-all duration-300 ${scrolled ? 'shadow-lg bg-white/95 backdrop-blur-sm' : 'bg-transparent'}`}>
             <NavLink to='/' className="transform hover:scale-110 transition-transform duration-300">
-                <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
+                <img src={mridul} alt='mridul' className='w-18 h-18 object-contain' />
             </NavLink>
             <nav className='flex text-lg gap-7 font-medium'>
                 <NavLink
